cba96ae2af68c8d7458c41e10162f149ff70ecfc
Ayush <ayushnigam072@gmail.com>
2025-03-13 17:59:20 +0530

    UC5: Manage Logging Levels Based on Environment

--- src/main/java/com/addressbook/addressbook/service/AddressBookService.java
+++ src/main/java/com/addressbook/addressbook/service/AddressBookService.java
@@ -15,29 +15,29 @@ public class AddressBookService {
     private final List<AddressBook> addressBookList = new ArrayList<>();
 
     public List<AddressBook> getAllContacts() {
-        log.info("Fetching all contacts");
+        log.debug("Fetching all contacts");
         return addressBookList;
     }
 
     public Optional<AddressBook> getContactById(int id) {
-        log.info("Fetching contact with ID: {}", id);
+        log.debug("Fetching contact with ID: {}", id);
         return addressBookList.stream()
                 .filter(contact -> contact.getId() == id)
                 .findFirst();
     }
 
     public AddressBook addContact(AddressBook contact) {
-        log.info("Adding new contact: {}", contact);
+        log.debug("Adding new contact: {}", contact);
         addressBookList.add(contact);
         return contact;
     }
 
     public Optional<AddressBook> updateContact(int id, AddressBook updatedContact) {
-        log.info("Updating contact with ID: {}", id);
+        log.debug("Updating contact with ID: {}", id);
         for (int i = 0; i < addressBookList.size(); i++) {
             if (addressBookList.get(i).getId() == id) {
                 addressBookList.set(i, updatedContact);
-                log.info("Updated contact: {}", updatedContact);
+                log.debug("Updated contact: {}", updatedContact);
                 return Optional.of(updatedContact);
             }
         }
@@ -46,10 +46,10 @@ public class AddressBookService {
     }
 
     public boolean deleteContact(int id) {
-        log.info("Deleting contact with ID: {}", id);
+        log.debug("Deleting contact with ID: {}", id);
         boolean removed = addressBookList.removeIf(contact -> contact.getId() == id);
         if (removed) {
-            log.info("Contact deleted successfully");
+            log.debug("Contact deleted successfully");
         } else {
             log.warn("Contact with ID {} not found for deletion", id);
         }
